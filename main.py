4fac987a62e70f6bd544577ba7f19ab3dd59d17e
Anmaya Panda <anmaya.panda1@gmail.com>
2024-11-08 23:46:28 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import env,sensors
+import math
+import pygame
+
+environment = env.buildEnvironment((600,1200))
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
